3a0cf8670bbc98108b0f519a7030342a21c0c81e
Dawid Ciepko <dciepko17@gmail.com>
2024-09-09 11:44:11 +0200

    Footer elements stylization

--- frontend/football-app/src/components/Header/Header.jsx
+++ frontend/football-app/src/components/Header/Header.jsx
@@ -1,10 +1,21 @@
 import classes from "./Header.module.css";
+import logo from "../../assets/logoprz2.jpg";
 
 export default function Header() {
   return (
     <header className={classes.container}>
-      <div className={classes.logoContainer}>LOGO</div>
-      <div className={classes.buttonContainer}>buttons</div>
+      <div className={classes.logoContainer}>
+        <img className={classes.logoPicture} src={logo} alt="Logo picture" />
+        <h1 className={classes.logoSign}>FootVision</h1>
+      </div>
+      <div className={classes.buttonContainer}>
+        <a className={classes.headerButton} href="#">
+          Zaloguj się
+        </a>
+        <a className={classes.headerButton} href="#">
+          Zarejestruj się
+        </a>
+      </div>
     </header>
   );
 }
