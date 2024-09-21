8ae001e46c95dbaaa4b93e4b03ac1ef39dec2886
Dawid Ciepko <dciepko17@gmail.com>
2024-09-21 20:08:54 +0200

    Creating ResultsBar

--- frontend/football-app/src/components/ResultBar/ResultBar.jsx
+++ frontend/football-app/src/components/ResultBar/ResultBar.jsx
@@ -3,7 +3,31 @@ import classes from "./ResultBar.module.css";
 export default function ResultBar({ fixture }) {
   return (
     <li className={classes.barItem}>
-      <button className={classes.barObject}>{fixture.teams.away.name}</button>
+      <button className={classes.barObject}>
+        <span className={classes.homePart}>
+          <div className={classes.logoContainer}>
+            <img
+              className={classes.logoImg}
+              src={fixture.teams.home.logo}
+              alt="Team logo"
+            />
+          </div>
+          <div>{fixture.teams.home.name}</div>
+          <div>{fixture.goals.home}</div>
+        </span>
+        -
+        <span className={classes.awayPart}>
+          <div>{fixture.goals.away}</div>
+          <div>{fixture.teams.away.name}</div>
+          <div className={classes.logoContainer}>
+            <img
+              className={classes.logoImg}
+              src={fixture.teams.away.logo}
+              alt="Team logo"
+            />
+          </div>
+        </span>
+      </button>
     </li>
   );
 }
