8ff5f4f52cb657c513c44ff23c8cba41b6927bad
Dawid Ciepko <dciepko17@gmail.com>
2024-09-22 14:12:15 +0200

    Importing leagues data

--- frontend/football-app/src/App.jsx
+++ frontend/football-app/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import HomePage from "./pages/HomePage/HomePage";
+import { useQuery } from "@tanstack/react-query";
 
 const App = () => {
   return (
