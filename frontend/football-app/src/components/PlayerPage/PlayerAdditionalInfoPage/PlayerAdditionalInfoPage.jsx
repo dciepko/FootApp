4d15c36dbea9756a7165d3c4a634708cca0bda66
Dawid Ciepko <dciepko17@gmail.com>
2024-10-09 20:16:24 +0200

    Implementing additional part of the player page

--- frontend/football-app/src/components/PlayerPage/PlayerAdditionalInfoPage/PlayerAdditionalInfoPage.jsx
+++ frontend/football-app/src/components/PlayerPage/PlayerAdditionalInfoPage/PlayerAdditionalInfoPage.jsx
@@ -1,5 +1,61 @@
 import classes from "./PlayerAdditionalInfoPage.module.css";
+import trophiesData from "../../../data/player/haalandTrophies.json";
+import transfersData from "../../../data/player/haalandTransfers.json";
 
 export default function PlayerAdditionalInfoPage() {
-  return;
+  const trophies = trophiesData;
+  const transfers = transfersData[0];
+
+  return (
+    <>
+      <div className={classes.transfersContainer}>
+        <h2>Transfers:</h2>
+        <div className={classes.transferList}>
+          <div className={classes.helpBarTransfers}>
+            <span>Date</span>
+            <span>From</span>
+            <span></span>
+            <span>To</span>
+            <span>Price</span>
+          </div>
+          <div className={classes.transfersList}>
+            {transfers.transfers.map((transfer) => {
+              return (
+                <li className={classes.singleTransfer}>
+                  <span>{transfer.date}</span>
+                  <span>{transfer.teams.out.name}</span>
+                  <span></span>
+                  <span>{transfer.teams.in.name}</span>
+                  <span>{transfer.type}</span>
+                </li>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+      <div className={classes.trophyContainer}>
+        <h2>Trophies:</h2>
+        <div className={classes.trophyList}>
+          <div className={classes.helpBarTrophies}>
+            <span>League</span>
+            <span>Country</span>
+            <span>Season</span>
+            <span>Place</span>
+          </div>
+          <div className={classes.trophiesList}>
+            {trophies.map((trophy) => {
+              return (
+                <li className={classes.singleTrophy}>
+                  <span>{trophy.league}</span>
+                  <span>{trophy.country}</span>
+                  <span>{trophy.season}</span>
+                  <span>{trophy.place}</span>
+                </li>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
