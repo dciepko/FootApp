b4fb2aad26f912fd6561b28aba76dc471d6363ba
Dawid Ciepko <dciepko17@gmail.com>
2024-10-16 19:38:28 +0200

    Displaying data on players page

--- frontend/football-app/src/components/MatchPage/MatchPlayersPage/MatchPlayersPage.jsx
+++ frontend/football-app/src/components/MatchPage/MatchPlayersPage/MatchPlayersPage.jsx
@@ -1,5 +1,33 @@
 import classes from "./MatchPlayersPage.module.css";
+import fixturesData from "../../../data/fixture.json";
 
 export default function MatchPlayersPage() {
-  return;
+  const matchData = fixturesData[0];
+  const playersData = matchData.lineups;
+
+  const renderContent = (teamLineup) => {
+    return (
+      <div className={classes.teamPlayers}>
+        <h2>{teamLineup.team.name}</h2>
+        <p>Formation:&nbsp;{teamLineup.formation}</p>
+        <p>Starting XI:</p>
+        {teamLineup.startXI.map((player) => {
+          return <li>{player.player.name}</li>;
+        })}
+        <p>Substitutes:&nbsp;</p>
+        {teamLineup.substitutes.map((player) => {
+          return <li>{player.player.name}</li>;
+        })}
+        <p>Coach:&nbsp;</p>
+        {teamLineup.coach.name}
+      </div>
+    );
+  };
+
+  return (
+    <div className={classes.playersPage}>
+      {renderContent(playersData[0])}
+      {renderContent(playersData[1])}
+    </div>
+  );
 }
