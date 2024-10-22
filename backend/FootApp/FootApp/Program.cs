ca09f3c5cdd85c1aea8a362340f898236d1894fd
Dawid Ciepko <dciepko17@gmail.com>
2024-10-22 18:24:43 +0200

    Adding AuthHelper

--- backend/FootApp/FootApp/Program.cs
+++ backend/FootApp/FootApp/Program.cs
@@ -5,10 +5,8 @@ using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddCors((options) =>
