54d57bb6707fe76b41cb6b022d96d917dab33ffb
Dawid Ciepko <dciepko17@gmail.com>
2024-10-22 21:33:32 +0200

    Applying charts for a player

--- frontend/football-app/src/components/PlayerPage/PlayerStatisticsPage/PlayerStatisticsPage.jsx
+++ frontend/football-app/src/components/PlayerPage/PlayerStatisticsPage/PlayerStatisticsPage.jsx
@@ -4,6 +4,8 @@ import seasons from "../../../data/player/haalandSeasons.json";
 import playerinfo from "../../../data/player/haaland.json";
 import DropdownOption from "../../DropdownOption/DropdownOption";
 import Pagination from "../../../components/Pagination/Pagination";
+import SimplePieChart from "../../Charts/SimplePieChart";
+import SimpleBarChart from "../../Charts/SimpleBarChart";
 
 export default function PlayerStatisticsPage() {
   const seasonsData = seasons;
@@ -45,17 +47,52 @@ export default function PlayerStatisticsPage() {
       position: currentCompetitionStats.games.position,
       rating: currentCompetitionStats.games.rating,
       captain: currentCompetitionStats.games.captain,
+      chart: (
+        <SimpleBarChart
+          data={[
+            {
+              name: "Appearences",
+              value: currentCompetitionStats.games.appearences,
+            },
+            {
+              name: "Lineups",
+              value: currentCompetitionStats.games.lineups,
+            },
+          ]}
+        />
+      ),
     },
     {
       statName: "Substitutes",
       in: currentCompetitionStats.substitutes.in,
       out: currentCompetitionStats.substitutes.out,
       bench: currentCompetitionStats.substitutes.bench,
+      chart: (
+        <SimplePieChart
+          data={[
+            { name: "In", value: currentCompetitionStats.substitutes.in },
+            { name: "Out", value: currentCompetitionStats.substitutes.out },
+          ]}
+        />
+      ),
     },
     {
       statName: "Shots",
       total: currentCompetitionStats.shots.total,
       on: currentCompetitionStats.shots.on,
+      chart: (
+        <SimplePieChart
+          data={[
+            { name: "On", value: currentCompetitionStats.shots.on },
+            {
+              name: "Missed",
+              value:
+                currentCompetitionStats.shots.total -
+                currentCompetitionStats.shots.on,
+            },
+          ]}
+        />
+      ),
     },
     {
       statName: "Goals",
@@ -63,6 +100,19 @@ export default function PlayerStatisticsPage() {
       conceded: currentCompetitionStats.goals.conceded,
       assists: currentCompetitionStats.goals.assists,
       saves: currentCompetitionStats.goals.assists,
+      chart: (
+        <SimpleBarChart
+          data={[
+            { name: "Total", value: currentCompetitionStats.goals.total },
+            {
+              name: "Conceded",
+              value: currentCompetitionStats.goals.conceded,
+            },
+            { name: "Assists", value: currentCompetitionStats.goals.assists },
+            { name: "Saves", value: currentCompetitionStats.goals.assists },
+          ]}
+        />
+      ),
     },
     {
       statName: "Passes",
@@ -75,27 +125,90 @@ export default function PlayerStatisticsPage() {
       total: currentCompetitionStats.tackles.total,
       blocks: currentCompetitionStats.tackles.blocks,
       interceptions: currentCompetitionStats.tackles.interceptions,
+      chart: (
+        <SimpleBarChart
+          data={[
+            { name: "Total", value: currentCompetitionStats.tackles.total },
+            {
+              name: "Blocks",
+              value: currentCompetitionStats.tackles.blocks,
+            },
+            {
+              name: "Interceptions",
+              value: currentCompetitionStats.tackles.interceptions,
+            },
+          ]}
+        />
+      ),
     },
     {
       statName: "Duels",
       total: currentCompetitionStats.duels.total,
       won: currentCompetitionStats.duels.won,
+      chart: (
+        <SimplePieChart
+          data={[
+            { name: "Won", value: currentCompetitionStats.duels.won },
+            {
+              name: "Lost",
+              value:
+                currentCompetitionStats.duels.total -
+                currentCompetitionStats.duels.won,
+            },
+          ]}
+        />
+      ),
     },
     {
       statName: "Dribbles",
       attempts: currentCompetitionStats.dribbles.attempts,
       success: currentCompetitionStats.dribbles.success,
       past: currentCompetitionStats.dribbles.past,
+      chart: (
+        <SimplePieChart
+          data={[
+            { name: "Won", value: currentCompetitionStats.dribbles.success },
+            {
+              name: "Lost",
+              value:
+                currentCompetitionStats.dribbles.attempts -
+                currentCompetitionStats.dribbles.success,
+            },
+          ]}
+        />
+      ),
     },
     {
       statName: "Fouls",
       drawn: currentCompetitionStats.fouls.drawn,
       committed: currentCompetitionStats.fouls.committed,
+      chart: (
+        <SimpleBarChart
+          data={[
+            { name: "Drawn", value: currentCompetitionStats.fouls.drawn },
+            {
+              name: "Committed",
+              value: currentCompetitionStats.fouls.committed,
+            },
+          ]}
+        />
+      ),
     },
     {
       statName: "Cards",
       yellow: currentCompetitionStats.cards.yellow,
       red: currentCompetitionStats.cards.red,
+      chart: (
+        <SimpleBarChart
+          data={[
+            { name: "Yellow", value: currentCompetitionStats.cards.yellow },
+            {
+              name: "Red",
+              value: currentCompetitionStats.cards.red,
+            },
+          ]}
+        />
+      ),
     },
     {
       statName: "Penalties",
@@ -104,6 +217,29 @@ export default function PlayerStatisticsPage() {
       scored: currentCompetitionStats.penalty.scored,
       missed: currentCompetitionStats.penalty.missed,
       saved: currentCompetitionStats.penalty.saved,
+      chart: (
+        <SimpleBarChart
+          data={[
+            { name: "Won", value: currentCompetitionStats.penalty.won },
+            {
+              name: "Committed",
+              value: currentCompetitionStats.penalty.commited,
+            },
+            {
+              name: "Scored",
+              value: currentCompetitionStats.penalty.scored,
+            },
+            {
+              name: "Missed",
+              value: currentCompetitionStats.penalty.missed,
+            },
+            {
+              name: "Saved",
+              value: currentCompetitionStats.penalty.saved,
+            },
+          ]}
+        />
+      ),
     },
   ];
 
@@ -143,17 +279,20 @@ export default function PlayerStatisticsPage() {
           <div className={classes.statisticDetails}>
             <h3>{currentStat.statName}</h3>
             <div className={classes.statisticDetailsList}>
-              {Object.entries(currentStat).map(([key, value]) => {
-                if (key !== "statName") {
-                  return (
-                    <div key={key} className={classes.statisticItem}>
-                      <strong>{key.toUpperCase()}:</strong>{" "}
-                      {value !== null ? value : "N/A"}
-                    </div>
-                  );
-                }
-                return null;
-              })}
+              <div className={classes.valuesContainer}>
+                {Object.entries(currentStat).map(([key, value]) => {
+                  if (key !== "statName") {
+                    return (
+                      <div key={key} className={classes.statisticItem}>
+                        <strong>{key.toUpperCase()}:</strong>{" "}
+                        {value !== null ? value : "N/A"}
+                      </div>
+                    );
+                  }
+                  return null;
+                })}
+              </div>
+              <div className={classes.chartContainer}>{currentStat.chart}</div>
             </div>
           </div>
         ) : (
