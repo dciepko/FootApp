6f2113dcc8f5ee2b692b1a606826e0de1284c3dc
Dawid Ciepko <dciepko17@gmail.com>
2024-10-22 17:46:50 +0200

    Creatint reposirtory for user

---
+++ backend/FootApp/FootApp/Data/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using FootApp.Models;
+
+namespace FootApp.Data
+{
+    public interface IUserRepository
+    {
+        IEnumerable<User> GetAllUsers();
+        User GetUserById(int userId);
+        bool UpdateUser(User user);
+        bool AddUser(User user);
+        bool DeleteUser(int userId);
+    }
+}
---
+++ backend/FootApp/FootApp/Data/UserRepository.cs
@@ -0,0 +1,70 @@
+﻿using FootApp.Models;
+
+namespace FootApp.Data
+{
+    public class UserRepository
+    {
+        private readonly DataContext _dbContext;
+
+        public UserRepository(DataContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            string sql = @"
+                SELECT [UserId],
+                    [FirstName],
+                    [LastName],
+                    [Email],
+                    [Active] 
+                FROM GoalVisionDB.Users";
+
+            return _dbContext.LoadData<User>(sql);
+        }
+
+        public User GetUserById(int userId)
+        {
+            string sql = @"
+                SELECT [UserId],
+                    [FirstName],
+                    [LastName],
+                    [Email],
+                    [Active] 
+                FROM GoalVisionDB.Users
+                WHERE UserId = @UserId";
+
+            return _dbContext.LoadDataSingle<User>(sql);
+        }
+
+        public bool UpdateUser(User user)
+        {
+            string sql = @"
+                UPDATE GoalVisionDB.Users
+                SET FirstName = @FirstName, 
+                    LastName = @LastName,
+                    Email = @Email, 
+                    Active = @Active
+                WHERE UserId = @UserId";
+
+            return _dbContext.ExecuteSql(sql);
+        }
+
+        public bool AddUser(User user)
+        {
+            string sql = @"
+                INSERT INTO GoalVisionDB.Users 
+                (FirstName, LastName, Email, Active) 
+                VALUES (@FirstName, @LastName, @Email, @Active)";
+
+            return _dbContext.ExecuteSql(sql);
+        }
+
+        public bool DeleteUser(int userId)
+        {
+            string sql = "DELETE FROM GoalVisionDB.Users WHERE UserId = @UserId";
+            return _dbContext.ExecuteSql(sql);
+        }
+    }
+}
