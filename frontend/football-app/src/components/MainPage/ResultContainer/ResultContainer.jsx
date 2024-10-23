3af4b28efc8ba0f24b699850cf90c18e065f104a
Dawid Ciepko <dciepko17@gmail.com>
2024-10-23 17:52:37 +0200

    Minor improvements

--- frontend/football-app/src/components/MainPage/ResultContainer/ResultContainer.jsx
+++ frontend/football-app/src/components/MainPage/ResultContainer/ResultContainer.jsx
@@ -4,24 +4,17 @@ import fixturesData from "../../../data/fixtures.json";
 import ResultBar from "../ResultBar/ResultBar";
 import { Link } from "react-router-dom";
 
-const fetchFixtures = async () => {
-  const url = "https://api-football-v1.p.rapidapi.com/v3/fixtures?live=all";
-  const options = {
-    method: "GET",
-    headers: {
-      "x-rapidapi-key": "ca33205c25msh1c3782cdb879e0ap1b6970jsnf69950db386a",
-      "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
-    },
-  };
-
-  const response = await fetch(url, options);
-
-  if (!response.ok) {
-    throw new Error("Error fetching the fixtures");
+// Funkcja pomocnicza do losowego wyboru 20 elementów z tablicy
+const getRandomFixtures = (fixtures, count) => {
+  // Kopia tablicy
+  let shuffled = [...fixtures];
+  // Algorytm Fisher-Yates do mieszania tablicy
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
   }
-
-  const result = await response.json();
-  return result.response;
+  // Zwracamy tylko 'count' pierwszych elementów
+  return shuffled.slice(0, count);
 };
 
 export default function ResultContainer() {
@@ -30,22 +23,26 @@ export default function ResultContainer() {
   //   queryKey: ["fixtures"],
   // });
 
+  // Sprawdzenie długości danych i wybór odpowiedniej ilości meczów
+  const fixturesToDisplay =
+    fixturesData.length <= 20
+      ? fixturesData
+      : getRandomFixtures(fixturesData, 20);
+
   return (
     <div className={classes.currentResultsList}>
-      {fixturesData &&
-        fixturesData.map((fixture, index) => {
-          const className = index % 2 === 0 ? "even" : "odd";
-          return (
-            <Link className="disablingLinks" to={"/match"}>
-              {" "}
-              <ResultBar
-                key={fixture.fixture.id}
-                fixture={fixture}
-                className={className}
-              />
-            </Link>
-          );
-        })}
+      {fixturesToDisplay.map((fixture, index) => {
+        const className = index % 2 === 0 ? "even" : "odd";
+        return (
+          <Link
+            className="disablingLinks"
+            to={"/match"}
+            key={fixture.fixture.id}
+          >
+            <ResultBar fixture={fixture} className={className} />
+          </Link>
+        );
+      })}
     </div>
   );
 }
